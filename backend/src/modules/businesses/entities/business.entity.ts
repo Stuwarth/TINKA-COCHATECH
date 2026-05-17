65176f4e54b7cf52dfd9e99b82bdcc07cd5b0981
Tomas Zapata Ortiz <tomaszapataortiz@gmail.com>
2026-05-16 23:39:49 -0400

    logueo iniciar sesion back

---
+++ backend/src/modules/businesses/entities/business.entity.ts
@@ -0,0 +1,12 @@
+export class Business {
+  id: string;
+  user_id: string;
+  name: string;
+  description?: string;
+  category?: string;
+  phone?: string;
+  location?: string;
+  status: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
